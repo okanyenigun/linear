0034bbff4aadee44149b1c4b6344b543e757d300
okanyeni <53236382+okanyenigun@users.noreply.github.com>
2021-12-21 19:29:34 +0300

    Update draft.py

--- draft.py
+++ draft.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
+import matplotlib.pyplot as plt
 from sklearn.datasets import load_boston
 #%% dataset
 boston_dataset = load_boston()
@@ -11,6 +12,7 @@ df['MEDV'] = boston_dataset.target
 #simple regression
 X = df["RM"]
 y = df["MEDV"]
+X = sm.add_constant(X)
 simple_model = sm.OLS(y, X).fit()
 predictions = simple_model.predict(X)
 print(simple_model.summary())
@@ -31,77 +33,128 @@ predictions = multi_model.predict(X)
 print(multi_model.summary())
 print("***********************")
 
-#%%
 
+#%% Ordinary Least Mean Square Method
 X = df["RM"]
+Y = df["MEDV"]
+# mean of our inputs and outputs
+x_mean = np.mean(X)
+y_mean = np.mean(Y)
+#total number of values
+n = len(X)
+# using the formula to calculate the b1 and b0
+numerator = 0
+denominator = 0
+for i in range(n):
+    numerator += (X[i] - x_mean) * (Y[i] - y_mean)
+    denominator += (X[i] - x_mean) ** 2
+    
+b1 = numerator / denominator
+b0 = y_mean - (b1 * x_mean)
+#printing the coefficient
+print(b1, b0)
+
+rmse = 0
+for i in range(n):
+    y_pred=  b0 + b1* X[i]
+    rmse += (Y[i] - y_pred) ** 2
+    
+rmse = np.sqrt(rmse/n)
+print(rmse)
+
+sumofsquares = 0
+sumofresiduals = 0
+for i in range(n) :
+    y_pred = b0 + b1 * X[i]
+    sumofsquares += (Y[i] - y_mean) ** 2
+    sumofresiduals += (Y[i] - y_pred) **2
+    
+score  = 1 - (sumofresiduals/sumofsquares)
+print(score)
+#%% GRADIENT DESCENT
+x = df["RM"]
 y = df["MEDV"]
 
-def mean(values):
-    return sum(values) / float(len(values))
-
-def variance(values,mean):
-    """The variance is the sum squared difference for each value from the mean value."""
-    return sum([(x-mean)**2 for x in values])
-
-def covariance(x,mean_x,mean_y):
-    """The covariance of two groups of numbers describes how those numbers change together."""
-    covar = 0.0
-    for i in range(len(x)):
-    	covar += (x[i] - mean_x) * (y[i] - mean_y)
-    return covar
-
-#%%
-
-def h(x,theta):
-    """
-    hθ(x) = θ0 + θ1x1 + θ2x2 + θ3x3 +…..+ θnxn
-    matmul: matrix multiplication
-    """
-    return np.matmul(x,theta)
-    
-def cost_function(x,y,theta):
-    """
-    
-    The @ operator can be used as a shorthand for np.matmul on ndarrays.
-    """
-    return ((h(x, theta)-y).T@(h(x, theta)-y))/(2*y.shape[0])
+x = (x - x.mean()) / x.std()
+x = np.c_[np.ones(x.shape[0]), x] 
 
 
-def gradient_descent(x,y,theta,learning_rate=0.1,num_epochs=10):
-    m = x.shape[0]
-    J_all = []
-    for _ in range(num_epochs):
-        h_x = h(x,theta)
-        cost_ = (1/m)*(x.T@(h_x-y))
-        theta = theta - (learning_rate)*cost_
-        J_all.append(cost_function(x,y,theta))
-    
-    return theta,J_all
+alpha = 0.01 #Step size
+iterations = 2000 #No. of iterations
+m = y.size #No. of data points
+np.random.seed(123) #Set the seed
+theta = np.random.rand(2) #Pick some random values to start with
 
-def add_constant(x):
-    ones = np.ones((X.shape[0],1))
-    return np.concatenate((x,ones),axis=1)
 
-#%%
-X = df.iloc[:,:13].values
-y = df["MEDV"]
-X = add_constant(X)
-theta = np.zeros((X.shape[1], 1))
-learning_rate = 0.1
-num_epochs = 50
-theta, J_all = gradient_descent(X, y, theta, learning_rate, num_epochs)
-J = cost_function(X, y, theta)
-print("Cost: ", J)
-print("Parameters: ", theta)
+#GRADIENT DESCENT
+def gradient_descent(x, y, theta, iterations, alpha):
+    past_costs = []
+    past_thetas = [theta]
+    for i in range(iterations):
+        prediction = np.dot(x, theta)
+        error = prediction - y
+        cost = 1/(2*m) * np.dot(error.T, error)
+        past_costs.append(cost)
+        theta = theta - (alpha * (1/m) * np.dot(x.T, error))
+        past_thetas.append(theta)
+        
+    return past_thetas, past_costs
 
-#for testing and plotting cost 
-n_epochs = []
-jplot = []
-count = 0
-for i in J_all:
-	jplot.append(i[0][0])
-	n_epochs.append(count)
-	count += 1
-jplot = np.array(jplot)
-n_epochs = np.array(n_epochs)
+#Pass the relevant variables to the function and get the new values back...
+past_thetas, past_costs = gradient_descent(x, y, theta, iterations, alpha)
+theta = past_thetas[-1]
 
+#Print the results...
+print("Gradient Descent: {:.2f}, {:.2f}".format(theta[0], theta[1]))
+
+#Plot the cost function...
+plt.title('Cost Function J')
+plt.xlabel('No. of iterations')
+plt.ylabel('Cost')
+plt.plot(past_costs)
+plt.show()
+
+#Animation
+
+#Set the plot up,
+import matplotlib.animation as animation
+fig = plt.figure()
+ax = plt.axes()
+plt.title('Sale Price vs Living Area')
+plt.xlabel('Living Area in square feet (normalised)')
+plt.ylabel('Sale Price ($)')
+plt.scatter(x[:,1], y, color='red')
+line, = ax.plot([], [], lw=2)
+annotation = ax.text(-1, 700000, '')
+annotation.set_animated(True)
+plt.close()
+
+#Generate the animation data,
+def init():
+    line.set_data([], [])
+    annotation.set_text('')
+    return line, annotation
+
+# animation function.  This is called sequentially
+def animate(i):
+    x = np.linspace(-5, 20, 1000)
+    y = past_thetas[i][1]*x + past_thetas[i][0]
+    line.set_data(x, y)
+    annotation.set_text('Cost = %.2f e10' % (past_costs[i]/10000000000))
+    return line, annotation
+
+anim = animation.FuncAnimation(fig, animate, init_func=init,
+                               frames=300, interval=0, blit=True)
+
+anim.save('animation.gif', writer='imagemagick', fps = 30)
+
+#Display the animation...
+import io
+import base64
+from IPython.display import HTML
+
+filename = 'animation.gif'
+
+video = io.open(filename, 'r+b').read()
+encoded = base64.b64encode(video)
+HTML(data='''<img src="data:image/gif;base64,{0}" type="gif" />'''.format(encoded.decode('ascii')))
